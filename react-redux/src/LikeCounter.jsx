14b486c6d7ad61f050719462b9aa8950c3eab7dd
Sahil Kharatmol <sahilkharatmol09@gmail.com>
2024-01-27 14:34:16 +0530

    done

---
+++ react-redux/src/LikeCounter.jsx
@@ -0,0 +1,32 @@
+import {Reducer} from './redux/Reducer'
+import { incrementLike, decrementLike } from './redux/Action'
+import {configureStore} from '@reduxjs/toolkit'
+import { Provider, useDispatch, useSelector } from 'react-redux'
+
+const store = configureStore({
+    reducer: Reducer
+});
+
+
+const LikeCounter = () => {
+
+    const likes = useSelector(state => state.likes);
+    const dispatch = useDispatch();
+
+    return(
+        <>
+            <h2>LIKE COUNTER : {likes}</h2>
+            <button onClick={()=>dispatch(incrementLike())}>Increment</button>
+            <button onClick={()=>dispatch(decrementLike())}>Decrement</button>
+        </>
+    )
+}
+
+const LikeCounterApp = () =>{
+    return(
+        <Provider store={store}>
+            <LikeCounter/>
+        </Provider>
+    )
+}
+export default LikeCounterApp
